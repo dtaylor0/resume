b24116b91411983d658fb720d43a9acb72c87cc3
Drew Taylor <drewtaylorm@gmail.com>
2024-10-19 10:43:04 -0400

    pages

---
+++ functions/index.ts
@@ -0,0 +1,113 @@
+import { Hono } from "hono";
+import { serveStatic } from "hono/cloudflare-workers";
+import { upgradeWebSocket } from "hono/cloudflare-workers";
+import {
+  CloudflareVectorizeStore,
+  CloudflareWorkersAIEmbeddings,
+} from "@langchain/cloudflare";
+import { MarkdownTextSplitter } from "langchain/text_splitter";
+import createRagChain from "@utils/llm";
+import { basicAuth } from "hono/basic-auth";
+import { Document } from "@langchain/core/documents";
+import { RunnableSequence } from "@langchain/core/runnables";
+
+type Bindings = {
+  AI: any;
+  VECTORIZE: VectorizeIndex;
+  USERNAME: string;
+  PASSWORD: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use("/", serveStatic({ root: "../dist/", manifest: {} }));
+
+app.get(
+  "/api/ws",
+  upgradeWebSocket((c) => {
+    return {
+      onMessage(event, ws) {
+        console.log("Received: %s", event.data);
+        const wsReq = JSON.parse(event.data);
+        if (!wsReq.prompt) {
+          console.log("No prompt found.");
+          ws.send(JSON.stringify({ response: "Womp" }));
+          return;
+        }
+        createRagChain(c.env.VECTORIZE, c.env.AI)
+          .then((r: RunnableSequence) => {
+            r.invoke(wsReq.prompt).then((llmRes) =>
+              ws.send(JSON.stringify({ response: llmRes })),
+            );
+          })
+          .catch((err: Error) => {
+            throw err;
+          });
+      },
+      onClose: () => {
+        console.log("Connection closed.");
+      },
+    };
+  }),
+);
+
+app.post(
+  "/api/resume",
+  async (c, next) => {
+    const auth = basicAuth({
+      username: c.env.USERNAME,
+      password: c.env.PASSWORD,
+    });
+    return auth(c, next);
+  },
+  async (c) => {
+    const resume = await c.req.json();
+    const ok = refreshVectorize(resume.contents, c.env.VECTORIZE, c.env.AI);
+    return c.json({ ok });
+  },
+);
+
+function createDocuments(text: string, chunkSize: number) {
+  if (text.length < chunkSize) {
+    throw new Error("Chunks cannot be larger than the input");
+  }
+
+  let documents = [];
+  let lastLoaded = 0;
+  while (lastLoaded < text.length - 1) {
+    const start = lastLoaded + 1;
+    let end = start + chunkSize;
+
+    while (end > start && text[end] != " ") {
+      end--;
+    }
+
+    const doc = new Document({ pageContent: text.slice(start, end) });
+    documents.push(doc);
+
+    lastLoaded = end;
+  }
+  return documents;
+}
+
+async function refreshVectorize(
+  contents: string,
+  vect: VectorizeIndex,
+  ai: Bindings["AI"],
+) {
+  const docs = createDocuments(contents, 600);
+  const md_splitter = new MarkdownTextSplitter({ chunkSize: 600 });
+  const splits = await md_splitter.splitDocuments(docs);
+
+  const embeddings = new CloudflareWorkersAIEmbeddings({
+    binding: ai,
+    model: "@cf/baai/bge-small-en-v1.5",
+  });
+  const store = new CloudflareVectorizeStore(embeddings, {
+    index: vect,
+  });
+
+  await store.addDocuments(splits);
+}
+
+export default app;
---
+++ utils/llm.ts
@@ -0,0 +1,57 @@
+import {
+    RunnableSequence,
+    RunnablePassthrough,
+} from "@langchain/core/runnables";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { PromptTemplate } from "@langchain/core/prompts";
+
+import {
+    CloudflareVectorizeStore,
+    CloudflareWorkersAI,
+    CloudflareWorkersAIEmbeddings,
+} from "@langchain/cloudflare";
+import type { VectorizeIndex } from "@cloudflare/workers-types";
+import { formatDocumentsAsString } from "langchain/util/document";
+
+export default async function createRagChain(
+    vectorIndex: VectorizeIndex,
+    ai: any,
+): Promise<RunnableSequence<any, string>> {
+    const llm = new CloudflareWorkersAI();
+    const embeddings = new CloudflareWorkersAIEmbeddings({
+        binding: ai,
+        model: "@cf/baai/bge-small-en-v1.5",
+    });
+    const vectorStore = new CloudflareVectorizeStore(embeddings, {
+        index: vectorIndex,
+    });
+
+    const retriever = vectorStore.asRetriever({
+        searchKwargs: { fetchK: 20 },
+    });
+
+    const prompt =
+        PromptTemplate.fromTemplate(`Your job is to answer questions about Drew's
+    resume for potential employers. Understand the context and then provide
+    the relevant answer to the question. You will not answer questions which
+    are unrelated to the resume, and you will not give specific estimates on
+    time, age, or any similar topic. You will also reformat all your
+    responses into well-formatted plaintext and no markdown. Additionally,
+    you will not quote anything directly from the resume.
+
+{context}
+
+Question: {question}
+
+Answer:`);
+
+    return RunnableSequence.from([
+        {
+            context: retriever.pipe(formatDocumentsAsString),
+            question: new RunnablePassthrough(),
+        },
+        prompt,
+        llm,
+        new StringOutputParser(),
+    ]);
+}
