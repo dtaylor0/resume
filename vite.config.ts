b0e0c19b1c6168da765b93e3f9179d93b5dfd94f
Drew Taylor <drewtaylorm@gmail.com>
2024-10-19 18:12:54 -0400

    maybe

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import devServer from "@hono/vite-dev-server";
+// import cloudflarePagesPlugin from "@hono/vite-cloudflare-pages";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  esbuild: {
+    platform: "node",
+  },
+  build: {
+    rollupOptions: {
+      external: ["@langchain/cloudflare"],
+    },
+  },
+  plugins: [
+    react(),
+    devServer({ entry: "src/main.jsx" }),
+    // cloudflarePagesPlugin(),
+  ],
+});
