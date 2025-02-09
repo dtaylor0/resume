fe6467b53f51f12a556f561f77b7e00d2fed1946
Drew Taylor <drewtaylorm@gmail.com>
2025-02-08 19:13:49 -0500

    fix layout for zoom

--- frontend/src/app/Chat.tsx
+++ frontend/src/app/Chat.tsx
@@ -1,46 +1,39 @@
-"use client";
+'use client';
 
-import React, {
-    FormEvent,
-    KeyboardEvent,
-    MutableRefObject,
-    useCallback,
-    useEffect,
-    useRef,
-    useState,
-} from "react";
-import ChatMessage from "./ChatMessage";
-import { Message } from "./ChatMessage";
+import React, { FormEvent, KeyboardEvent, MutableRefObject, useCallback, useEffect, useRef, useState } from 'react';
+import ChatMessage from './ChatMessage';
+import { Message } from './ChatMessage';
 
 function Chat() {
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => setIsClient(true), []);
+
     const [messages, setMessages] = useState(() => {
-        if (typeof window === "undefined") {
+        if (!isClient) {
             return [] as Message[];
         }
-        const savedMessages = localStorage?.getItem("messages");
-        return savedMessages !== null
-            ? (JSON.parse(savedMessages) as Message[])
-            : ([] as Message[]);
+        const savedMessages = localStorage.getItem('messages');
+        return savedMessages !== null ? (JSON.parse(savedMessages) as Message[]) : ([] as Message[]);
     });
-    useEffect(
-        () => localStorage?.setItem("messages", JSON.stringify(messages)),
-        [messages],
-    );
-    const textareaRef: MutableRefObject<HTMLTextAreaElement | null> =
-        useRef(null);
-    const chatContainerRef: MutableRefObject<HTMLDivElement | null> =
-        useRef(null);
+
+    useEffect(() => {
+        const chatContainer = chatContainerRef.current;
+        if (chatContainer) {
+            chatContainer.scrollTop = chatContainer.scrollHeight;
+        }
+    }, [messages]);
+
+    useEffect(() => localStorage.setItem('messages', JSON.stringify(messages)), [messages]);
+    const textareaRef: MutableRefObject<HTMLTextAreaElement | null> = useRef(null);
+    const chatContainerRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
 
     const ws: MutableRefObject<WebSocket | null> = useRef(null);
     useEffect(() => {
-        ws.current = new WebSocket("/api/v1/ws");
+        ws.current = new WebSocket('/api/v1/ws');
         ws.current.onmessage = (event) => {
             const json = JSON.parse(event.data);
             console.log(json);
-            setMessages((ms) => [
-                ...ms,
-                { sender: "server", text: json.response },
-            ]);
+            setMessages((ms) => [...ms, { sender: 'server', text: json.response }]);
             if (textareaRef.current) {
                 textareaRef.current.focus();
             }
@@ -52,34 +45,17 @@ function Chat() {
         };
     }, []);
 
-    useEffect(() => {
-        const chatContainer = chatContainerRef.current;
-        if (chatContainer) {
-            chatContainer.scrollTop = chatContainer.scrollHeight;
-        }
-    }, [messages]);
-
     const handleSubmit = useCallback(
         (e: KeyboardEvent | FormEvent) => {
             e.preventDefault();
 
-            if (
-                messages.length > 0 &&
-                messages[messages.length - 1].sender === "client"
-            ) {
-                console.log(
-                    "wait for the llm to respond before sending another message.",
-                );
+            if (messages.length > 0 && messages[messages.length - 1].sender === 'client') {
+                console.log('wait for the llm to respond before sending another message.');
                 return;
             }
-            const promptInput = document.getElementById(
-                "prompt-input",
-            ) as HTMLTextAreaElement;
+            const promptInput = document.getElementById('prompt-input') as HTMLTextAreaElement;
             const prompt = promptInput.value;
-            setMessages((msgs) => [
-                ...msgs,
-                { sender: "client", text: prompt },
-            ]);
+            setMessages((msgs) => [...msgs, { sender: 'client', text: prompt }]);
             if (ws.current && ws.current.readyState === WebSocket.OPEN) {
                 ws.current.send(
                     JSON.stringify({
@@ -87,13 +63,10 @@ function Chat() {
                     }),
                 );
 
-                promptInput!.value = "";
+                promptInput!.value = '';
             } else {
-                console.log("WebSocket is not connected");
-                setMessages((msgs) => [
-                    ...msgs,
-                    { sender: "server", text: "Not connected to the server." },
-                ]);
+                console.log('WebSocket is not connected');
+                setMessages((msgs) => [...msgs, { sender: 'server', text: 'Not connected to the server.' }]);
             }
         },
         [messages],
@@ -101,7 +74,7 @@ function Chat() {
 
     const handleKeyDown = useCallback(
         (e: KeyboardEvent) => {
-            if (e.key === "Enter" && e.shiftKey == false) {
+            if (e.key === 'Enter' && e.shiftKey == false) {
                 e.preventDefault();
                 handleSubmit(e);
             }
@@ -115,11 +88,7 @@ function Chat() {
                 <button
                     className="p-2 my-auto float-right font-semibold bg-background border-2 border-accent rounded-lg ml-auto h-10"
                     onClick={() => {
-                        if (
-                            confirm(
-                                "Are you sure you want to erase the current chat history?",
-                            )
-                        ) {
+                        if (confirm('Are you sure you want to erase the current chat history?')) {
                             setMessages([]);
                         }
                     }}
@@ -128,28 +97,19 @@ function Chat() {
                 </button>
             </div>
             <div id="chat" className="h-full rounded-lg mt-3">
-                <div
-                    ref={chatContainerRef}
-                    className="h-[70%] overflow-y-auto scrollbar-thin"
-                >
+                <div ref={chatContainerRef} className="h-[70%] overflow-y-auto scrollbar-thin">
                     <p className="text-sm md:text-base overflow-hidden">
-                        My name is Drew and I am a Software Engineer at
-                        Deloitte. I implement cloud solutions for commercial and
-                        federal clients under the Deloitte AI & Engineering
-                        Offering. Please use the chat below to learn more about
-                        my work experience.
+                        My name is Drew and I am a Software Engineer at Deloitte. I implement cloud solutions for commercial and federal
+                        clients under the Deloitte AI & Engineering Offering. Please use the chat below to learn more about my work
+                        experience.
                     </p>
                     {messages.map((msg, i) => ChatMessage(msg, i))}
                 </div>
-                <form
-                    id="chat-form"
-                    className="rounded-lg flex bg-background p-3"
-                    onSubmit={handleSubmit}
-                >
+                <form id="chat-form" className="rounded-lg flex bg-background px-3" onSubmit={handleSubmit}>
                     <textarea
                         id="prompt-input"
                         placeholder="Type your question here..."
-                        className="flex-grow scrollbar-thin rounded-lg bg-inherit focus:outline-none resize-none p-3 mx-3 h-25"
+                        className="flex-grow scrollbar-thin rounded-lg bg-inherit focus:outline-none resize-none py-1 px-3 mx-3"
                         ref={textareaRef}
                         onKeyDown={handleKeyDown}
                         inputMode="text"
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,11 @@
-import Header from "./Header";
-import Chat from "./Chat";
+import Header from './Header';
+import Chat from './Chat';
 
 function App() {
     return (
-        <div className="h-screen flex flex-col">
+        <div className="h-svh flex flex-col">
             <Header />
-            <div
-                id="content"
-                className="w-[90%] md:w-[75%] lg:w-1/2 h-[80%] mx-auto my-3 px-3 md:px-12 py-5 rounded-xl bg-altbackground"
-            >
+            <div id="content" className="w-[90%] md:w-[75%] lg:w-1/2 h-[80%] mx-auto my-3 px-3 md:px-12 py-5 rounded-xl bg-altbackground">
                 <Chat />
             </div>
         </div>
