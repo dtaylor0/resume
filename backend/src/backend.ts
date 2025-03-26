c63e333b1af8516d21055bdf8d71f250ed6887d2
dtaylor0 <drewtaylorm@gmail.com>
2025-03-26 18:12:10 -0400

    chat

--- backend/src/backend.ts
+++ backend/src/backend.ts
@@ -1,14 +1,9 @@
 import { Hono } from "hono";
 import { upgradeWebSocket } from "hono/cloudflare-workers";
-import {
-    CloudflareVectorizeStore,
-    CloudflareWorkersAIEmbeddings,
-} from "@langchain/cloudflare";
-import { MarkdownTextSplitter } from "langchain/text_splitter";
-import streamRagChain from "./llm.js";
-import { Document } from "@langchain/core/documents";
+import streamChain from "./llm.js";
 import { randomUUID } from "crypto";
 import { AIMessage, HumanMessage } from "@langchain/core/messages";
+import { BaseMessagePromptTemplateLike } from "@langchain/core/prompts";
 
 type Bindings = {
     AI: any;
@@ -43,21 +38,21 @@ app.get(
                         return;
                     }
 
-                    const messages = wsReq.map((m: MessageData) => {
-                        switch (m.sender) {
-                            case "human":
-                                return new HumanMessage(m.text);
-                            case "ai":
-                                return new AIMessage(m.text);
-                            default:
-                                return new HumanMessage(m.text);
-                        }
-                    });
+                    const messages: BaseMessagePromptTemplateLike[] = wsReq.map(
+                        (m: MessageData) => {
+                            switch (m.sender) {
+                                case "human":
+                                    return new HumanMessage(m.text);
+                                case "ai":
+                                    return new AIMessage(m.text);
+                                default:
+                                    return new HumanMessage(m.text);
+                            }
+                        },
+                    );
 
                     const id = randomUUID();
-                    streamRagChain(
-                        c.env.VECTORIZE,
-                        c.env.AI,
+                    streamChain(
                         c.env.CF_ACCOUNT_ID,
                         c.env.CF_API_TOKEN,
                         messages,
@@ -70,7 +65,7 @@ app.get(
                                 done = chunk.done;
                                 const msg = {
                                     id,
-                                    text: chunk.value?.answer,
+                                    text: chunk.value,
                                     done,
                                 };
                                 ws.send(JSON.stringify(msg));
@@ -94,48 +89,5 @@ app.get(
         };
     }),
 );
-
-app.post("/api/v1/resume", async (c) => {
-    const resume = await c.req.json();
-    const ok = await refreshVectorize(
-        resume.contents,
-        c.env.VECTORIZE,
-        c.env.AI,
-    );
-    return c.json({ ok });
-});
-
-async function refreshVectorize(
-    contents: string,
-    vect: VectorizeIndex,
-    ai: Bindings["AI"],
-) {
-    const md_splitter = new MarkdownTextSplitter();
-    const splits = await md_splitter.splitText(contents);
-    for (let i = 0; i < splits.length; i++) {
-        for (const [k, v] of Object.entries(splits[i])) {
-            if (typeof v === "object") {
-                splits[i][k] = JSON.stringify(v);
-            }
-        }
-    }
-
-    const embeddings = new CloudflareWorkersAIEmbeddings({
-        binding: ai,
-        model: "@cf/baai/bge-small-en-v1.5",
-    });
-    const store = new CloudflareVectorizeStore(embeddings, {
-        index: vect,
-    });
-
-    splits.forEach((pageContent) =>
-        console.log(
-            `PAGE CONTENT:\n${pageContent}\n--------------------------\n`,
-        ),
-    );
-    await store.addDocuments(
-        splits.map((pageContent) => new Document({ pageContent })),
-    );
-}
 
 export default app;
--- backend/src/llm.ts
+++ backend/src/llm.ts
@@ -1,84 +1,37 @@
 import {
+    BaseMessagePromptTemplateLike,
     ChatPromptTemplate,
-    MessagesPlaceholder,
 } from "@langchain/core/prompts";
 
-import {
-    CloudflareVectorizeStore,
-    CloudflareWorkersAI,
-    CloudflareWorkersAIEmbeddings,
-} from "@langchain/cloudflare";
-import type { VectorizeIndex, Ai } from "@cloudflare/workers-types";
-import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
-import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
-import { createRetrievalChain } from "langchain/chains/retrieval";
-import { AIMessage, HumanMessage } from "@langchain/core/messages";
+import { CloudflareWorkersAI } from "@langchain/cloudflare";
 import { IterableReadableStream } from "@langchain/core/utils/stream";
-import { Document } from "langchain/document";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { context } from "../resumeContent.js";
 
-export default async function streamRagChain(
-    vectorIndex: VectorizeIndex,
-    ai: Ai,
+export default async function streamChain(
     cfAcctId: string,
     cfApiToken: string,
-    msgs: (HumanMessage | AIMessage)[],
-): Promise<
-    IterableReadableStream<{
-        context: Document<Record<string, any>>[];
-        answer: string;
-    }>
-> {
+    msgs: BaseMessagePromptTemplateLike[],
+): Promise<IterableReadableStream<string>> {
     const llm = new CloudflareWorkersAI({
         model: "@cf/meta/llama-3.3-70b-instruct-fp8-fast", // @cf/meta/llama-3.1-70b-instruct
         cloudflareAccountId: cfAcctId,
         cloudflareApiToken: cfApiToken,
     });
-    const embeddings = new CloudflareWorkersAIEmbeddings({
-        binding: ai,
-        model: "@cf/baai/bge-small-en-v1.5",
-    });
-    const vectorStore = new CloudflareVectorizeStore(embeddings, {
-        index: vectorIndex,
-    });
 
-    const retriever = vectorStore.asRetriever();
+    const systemPrompt = `Your job is to answer questions about Drew's resume. You will only answer the question using the given context from the resume. Respond in a conversational manner. Context: ${context}`;
+    const sysMsg: BaseMessagePromptTemplateLike = ["system", systemPrompt];
+    const messages = [sysMsg, ...msgs];
+    const prompt = ChatPromptTemplate.fromMessages(messages);
 
-    const systemPrompt = `Your job is to answer questions about Drew's resume. You will only answer the question using the given context from the resume. Respond in a conversational manner. Context: {context}`;
-    const prompt = ChatPromptTemplate.fromMessages([
-        ["system", systemPrompt],
-        ["user", "{input}"],
+    const chain = RunnableSequence.from([
+        prompt,
+        llm,
+        new StringOutputParser(),
     ]);
 
-    const documentChain = await createStuffDocumentsChain({
-        llm,
-        prompt,
-    });
-
-    const retrieverChain = await createHistoryAwareRetriever({
-        llm,
-        retriever,
-        rephrasePrompt: ChatPromptTemplate.fromMessages([
-            new MessagesPlaceholder("messages"),
-            [
-                "user",
-                "Given the above conversation, generate a search query to look up in order to provide additional context for the conversation. The most recent question is: {input}",
-            ],
-            [
-                "system",
-                "The search query should be very concise and simple. It should exclude any mention of the conversation history.",
-            ],
-        ]),
-    });
-
-    const retrievalChain = await createRetrievalChain({
-        combineDocsChain: documentChain,
-        retriever: retrieverChain,
-    });
-
-    const stream = await retrievalChain.stream({
-        input: msgs[msgs.length - 1].content as string,
-        messages: msgs,
-    });
+    const stream = await chain.stream({});
 
     return stream;
 }
